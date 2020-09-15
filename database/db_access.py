c4f386ed1c5da05c866c5903563ed8a95ed674f0
Audrey <audrey.cornaire@gmail.com>
2020-09-15 10:16:13 +0200

    [CORRET] - Réparation du merge qui pue.

--- database/db_access.py
+++ database/db_access.py
@@ -140,7 +140,7 @@ class DatabaseManager:
         Returns:
             json: top positives tweets
         """
-        result = self.__get_collection('smart_tweets', 'tweets').find(filters).count(5)
+        result = self.__get_collection('smart_tweets', 'tweets').find(filters).sort([("sentiment.confidence_score.positive", 1)]).limit(5)
         return result
 
     def get_top_negative_tweets(self, filters):
@@ -149,7 +149,7 @@ class DatabaseManager:
         Returns:
             json: top negative tweets
         """
-        result = self.__get_collection('smart_tweets', 'tweets').requete.count(5)
+        result = self.__get_collection('smart_tweets', 'tweets').find(filters).sort([("sentiment.confidence_score.negative", 1)]).limit(5)
         return result
         
 
